8abf40b256124ad8ab9c7e7933ab1f2814f5a48e
myanh2012 <myanh.ngnc@gmail.com>
2026-01-19 16:47:12 -0800

    search by name added

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -38,12 +38,25 @@ app.get("/", (req, res) => {  // root
   res.send("Hello World!");
 });
 
-app.get("/users", (req, res) => { // ../users (like a 2nd tab)
-  res.send(users); 
-});
-
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
   );
+});
+
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
 });
